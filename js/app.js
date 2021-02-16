19593102c0810571fbfef9c8c4c1516e980e29bd
aseel-farrar <aseelfarrar1@hotmail.com>
2021-02-16 23:53:33 +0200

    lab 03 1st verion

--- js/app.js
+++ js/app.js
@@ -3,6 +3,99 @@
 let userName = prompt('Please enter your name');
 alert('Welcome to my site '+ userName +' and I am pleased because you visit my site and we going to play the guessing game together');
 
+
+//lab 03
+let attempt = 1;
+let attemptNum;
+let isValidAttempts= true;
+let mark = 0;
+
+// // 6th question: How much do I weigh?
+let weight;
+let myWeight = 46;
+attemptNum = 4;
+
+do {
+  weight = prompt('How much do I weigh?');
+  isValidAttempts = attemptsCount ();
+
+  if (isNumber(weight) && Number(weight) !== myWeight){
+    console.log(numStatus(weight, myWeight));
+  }
+  else if (Number(weight) === myWeight){
+    alert('Right Answer');
+    mark ++;
+  }
+} while(isValidAttempts && Number(weight) !== myWeight);
+
+//7th question: Give me one of the prime factors of 210?
+// let primeFactors = [2, 3, 5, 7];
+// let userPrimeFactor;
+// let isArrayElement = false;
+// attemptNum = 6;
+
+
+// userPrimeFactor = prompt('Give me one of the prime factors of 210?');
+// isValidAttempts = attemptsCount();
+
+// while () {
+//   for(let i = 0; i < 4; i++){
+//     if (userPrimeFactor === primeFactors[i]) {
+//       console.log('GOOD, Right Answer ...' + primeFactors);
+//       isArrayElement = true;
+//     }
+
+//     if (isArrayElement === true) {
+//       console.log(' yes it is array element i will break!!');
+//       break;
+//     }
+
+//   }
+//   console.log('test');
+// }
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// function to ckech the number is it too high OR too low
+function numStatus(userInput, ans){
+  if (userInput < (ans - 5) && attempt !== 1)
+    return alert('Too Low');
+  else if (userInput > (ans + 5) && attempt !== 1)
+    return alert('Too High');
+  else if (attempt === 1){
+    alert('no more attempts for this question the Right answer is ---> ' + ans);
+  }
+}
+
+// function to check if the user enter a number
+function isNumber(num){
+  if(isNaN(num)){
+    console.log(isNaN(num));
+    alert('Please enter a number');
+    return false;
+  }
+  else
+    return true;
+}
+
+//function to count is there more attempts
+function attemptsCount (){
+  if (attempt < attemptNum) {
+    attempt++;
+    return true;
+  }
+  else{
+    attempt = 1;
+    return false;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////
+
+
+// lab 02
+
 //Do I have a university degree?
 let degree = prompt('Do I have a university degree?').toUpperCase();
 console.log('The user asnwer "Do I have a university degree?" ------> '+ degree);
@@ -17,6 +110,7 @@ case 'YES':
 case 'Y':
   // console.log('Yes right answer!!  ^_^ \n I have BSc degree in Computer engineering from Jordan University, also I have diploma degree in IT from WSTC.');
   alert('Yes right answer!!  ^_^ \n I have BSc degree in Computer engineering from Jordan University, also I have diploma degree in IT from WSTC.');
+  mark++;
   break;
 case 'NO':
 case 'N':
@@ -44,6 +138,7 @@ case 'NO':
 case 'N':
   // console.log('Yes right answer!!  ^_^ \n I am 27 years old and My birthday is the same as International Women Day 8th march.');
   alert('Yes right answer!!  ^_^ \n I am 27 years old and My birthday is the same as International Women Day 8th march.');
+  mark++;
   break;
 }
 
@@ -61,6 +156,7 @@ case 'YES':
 case 'Y':
   // console.log('Yes right answer!!  ^_^  \n I enjoy programming especially when I design websites');
   alert('Yes right answer!!  ^_^  \n I enjoy programming especially when I design websites');
+  mark++;
   break;
 case 'NO':
 case 'N':
@@ -82,6 +178,7 @@ switch (passion) {
 case 'YES':
 case 'Y':
   // console.log('Yes right answer!!  ^_^  \n I have a passion for learning and master new skills, and Continuously develop and improve my current knowledge and skills');
+  mark++;
   alert('Yes right answer!!  ^_^  \n I have a passion for learning and master new skills, and Continuously develop and improve my current knowledge and skills');
   break;
 case 'NO':
@@ -106,6 +203,7 @@ case 'YES':
 case 'Y':
   // console.log('Yes right answer!!  ^_^  \n I love volunteering especially with visual impairment people.');
   alert('Yes right answer!!  ^_^  \n I love volunteering especially with visual impairment people.');
+  mark++;
   break;
 case 'NO':
 case 'N':
@@ -113,4 +211,4 @@ case 'N':
   alert('NO wrong answer!! -_-\n I love volunteering especially with visual impairment people.');
   break;
 }
-alert('Good-bye '+ userName +' and  I wish you enjoyed playing the guessing game together ^_^');
+alert('Good-bye '+ userName +' and  I wish you enjoyed playing the guessing game together ^_^ \n you got: ' + mark +' of 7');
